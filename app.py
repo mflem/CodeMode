2ddc71951229bd1fa6c7afeee1b4be4d6b4b85f8
mmjenjen <mjennin3@wellesley.edu>
2017-11-29 12:16:07 -0500

    insert??

--- app.py
+++ app.py
@@ -18,18 +18,12 @@ def home():
 @app.route('/select/', methods =['POST', 'GET'])
 # page for selecting a deck to quiz on
 def select():
-    conn = movie.getConn()
-    movies = movie.get_movies(conn)
-    if request.method == 'POST':
-        movie_tt = request.form['menu-tt']
-        return redirect(url_for('update', id=movie_tt))
     return render_template('select.html')
 
 @app.route('/make/', methods =['POST', 'GET'])
 # page for making questions
 def make():
-    conn = movie.getConn()
-    movie_data = movie.find_movie(conn, int(id))
+
     return render_template('make.html', data=update_info)
 
 app.secret_key = 'youcantguessthisout'
@@ -37,7 +31,24 @@ app.secret_key = 'youcantguessthisout'
 @app.route('/quiz/<id>')
 #page for taking a quiz
 def quiz(id):
-    return render_template('quiz.html')
+    if request.method == 'POST': # if there is a request
+        action = request.form['submit']
+        if action == 'add':
+            #gathers inputted info to send to database
+            questionText = request.form['questionText']
+            answer = request.form['answer']
+            qtype = request.form['qtype']
+            wrong1 = request.form['wrong1']
+            wrong2 = request.form['wrong2']
+            wrong3 = request.form['wrong3']
+            explanation = request.form['explanation']
+            pointVal = request.form['pointVal']
+            deckNum = request.form['deckNum']
+            data = (questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, pointVal, deckNum)
+            codemodeFunctions.update(conn,data)
+            # throw in redirect to update page
+    else:
+        return render_template('quiz.html')
 
 if __name__ == '__main__':
   app.debug == True
--- codemodeFunctions.py
+++ codemodeFunctions.py
@@ -2,5 +2,21 @@
 # CodeMode
 # Draft 11/30/17
 
+import sys
+import MySQLdb
+import dbconn2
+from flask import flash
+
 #functions and connections necessary for app.py
-#??
+
+def update_movie(conn, data):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    try:
+        # data = (questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, pointVal, deckNum)
+        curs.execute('''INSERT INTO questions
+        (questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, pointVal, deckNum)
+                        ''', (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]))
+            to_flash = "Question (" + str(data[0]) +") was inserted successfully"
+            flash(to_flash)
+    except Exception as error:
+        flash("error: {}".format(error))
