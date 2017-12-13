12438ac7dde2bd2ac908f4288299a2e219a801b7
mmjenjen <mjennin3@wellesley.edu>
2017-12-12 19:18:00 -0500

    grading??

--- grading.js
+++ grading.js
@@ -3,9 +3,14 @@ function grading(){
     var radioVal = $("input[name='quiz']:checked").val();
     var textVal = $("input[name='quiz'][type='text']").val();
     var correctAnswer = $('p').attr('data-answer');
+    console.log(radioVal);
+    console.log(textVal);
+    console.log(correctAnswer);
     if (radioVal == correctAnswer || textVal == correctAnswer){
       $('questionResult').text('Correct!');
+      console.log("Correct!");
     } else{
       $('questionResult').text('Wrong!');
+      console.log('Wrong!');
     }
 }
