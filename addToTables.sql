91826b32adb4d4c48a0394b64fc256f57921682e
mmjenjen <mjennin3@wellesley.edu>
2017-12-14 00:02:03 -0500

    add two new decks

--- addToTables.sql
+++ addToTables.sql
@@ -32,16 +32,38 @@ VALUES("Which is a relative url?",
 	"www.google.com/kitten.png", "Desktop/user/other/kitten.png",
 	'''The correct answer is /kitten.png because it does not specify the whole path.',1,"HTML Questions")
 
-/* testing madeby */
-insert into madeBy(qid, cid) values
-		('1','2'),
-		('3','2');
+/* SQL DECK */
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("What does SQL stand for?","Structured Query Language", "multi", "Structured Question Language", "Strong Query Language", "Sanctioned Query Language", "SQL stands for Structured Query Language",1,"SQL")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("Which SQL statement is used to update data in a database?","UPDATE", "multi", "INSERT", "MODIFY", "SAVE AS", "UPDATE is the keyword to update data in SQL.",1,"SQL")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("Which SQL statement is used to insert new data in a database?","INSERT INTO", "fillblank", "", "", "", "You use INSERT INTO to insert data into a table",2,"SQL")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("With SQL, how do you select all the columns from a table named Persons?",
+  	"SELECT * FROM Persons", "multi", "SELECT Persons",
+  	"SELECT *.Persons", "SELECT [all] FROM Persons",
+  	"SELECT * FROM Persons is the only valid answer.",3,"SQL")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("With SQL, how do you select a column named "FirstName" from a table named "Persons"?",
+  	"SELECT FirstName FROM Persons", "multi", "EXTRACT FirstName FROM Persons",
+  	"SELECT Persons.FirstName", "INSERT INTO Persons FirstName",
+  	"In SQL, you use the SELECT keyword to select columns from a table.",4,"SQL")
 
-/* fun additions */
-insert into questions(questionText, answer) values ('Is pi delicious?','Obviously');
-insert into questions(questionText, answer) values ('How much wood would a woodchuck, chuck, if a woodchuck was Chuck Norris?', 'Much');
-
-/* Testing updating */
-UPDATE questions SET wrong2 = 'Hardly' WHERE qid=1;
-UPDATE questions SET wrong1 = '1' WHERE qid=2;
-UPDATE questions SET wrong2 = '2' WHERE qid=2;
+/* PHP DECK */
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("What does PHP stand for?","PHP: Hypertext Preprocessor", "multi", "Private Home Page", "Personal Hypertext Processor", "Pigs Hugging Pugs", "",1,"PHP")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("PHP server scripts are surrounded by delimiters, which?","<?php...?>", "multi", "<script>...</script>", "<&>...</&>", "<?php>...</?>", "PHP is delimitted by <?php...?>",1,"PHP")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("How do you write "Hello World" in PHP?","echo 'Hello World';", "multi", "Document.Write('Hello World');", "Hello World", "Hello.Word", "echo is the proper way to put things back on a page.",2,"PHP")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("All variables in PHP start with which symbol?",
+  	"$", "fillblank", "#",
+  	"@", "%",
+  	"All variables in PHP start with $.",3,"PHP")
+  INSERT INTO questions(questionText, answer, qtype, wrong1, wrong2, wrong3, explanation, point_value, deck_name)
+  VALUES("How do you get information from a form that is submitted using the "get" method?",
+  	"$_GET[];", "multi", "$GET_REQUEST",
+  	"Request.Form;", "Request.QueryString;",
+  	"$_GET[]; is how you get information from the GET method request.",5,"PHP")
